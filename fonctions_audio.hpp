9bffa73e2086988833236836f5598f08bb495a12
geraphael <raphael.geay@ynov.com>
2025-01-28 11:44:06 +0100

    sonswav

---
+++ fonctions_audio.hpp
@@ -0,0 +1,13 @@
+#ifndef FONCTIONS_AUDIO_HPP
+#define FONCTIONS_AUDIO_HPP
+
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <string>
+#include <map>
+
+Mix_Chunk* loadSound(const std::string& file);
+void playNote(const std::map<std::string, Mix_Chunk*>& notes, const std::string& key);
+int mode_interactif();
+
+#endif
